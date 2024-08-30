91856b87d0b20024b121f7e2dc637cb1a6a70801
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-08-30 18:31:18 +0300

    data-fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
-import AddCompanyButton from './components/add-company-button';
-import ClientComponent from './components/client-component';
-import MagicButton from './components/magic-button';
-import ServerComponent from './components/server-component';
-import ServerComponentCopy from './components/server-component-copy';
-import StatusLabel, { Status } from './components/status-label';
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home Page</h1>
-      <AddCompanyButton />
-      {/* <MagicButton /> */}
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
+
+// import AddCompanyButton from './components/add-company-button';
+// import ClientComponent from './components/client-component';
+// import MagicButton from './components/magic-button';
+// import ServerComponent from './components/server-component';
+// import ServerComponentCopy from './components/server-component-copy';
+// import StatusLabel, { Status } from './components/status-label';
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1 className="text-xl">Home Page</h1>
+//       <AddCompanyButton />
+//       {/* <MagicButton /> */}
+//     </main>
+//   );
+// }
